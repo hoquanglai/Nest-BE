619614fa7acb89799095cc52184c35ebf38ca649
QUANGLAI\MSI VN <hquanglai@gmail.com>
2023-02-24 00:40:11 +0700

    base controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BaseModule } from './base/base.module';
+import { AuthModule } from './auth/auth.module';
+import { S3Module } from './s3/s3.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '123456',
+      database: 'db',
+    }),
+    BaseModule,
+    AuthModule,
+    S3Module,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
